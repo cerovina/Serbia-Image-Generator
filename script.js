1186aae7b98270a923d691137af32b8a4dd0a460
Nikola Cerovina <cerovina1997@gmail.com>
2023-06-04 18:52:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const button = document.getElementById("generate-button");
+const imageContainer = document.getElementById("image-container");
+
+const imageUrls = [
+  "Tara.jpg",
+  "Golubac.jpg",
+  "oj.jpg",
+  "sarganska-osmica.jpg",
+  "begis.jpg",
+  "ns.jpg",
+  "brda1.jpg"
+];
+
+let currentIndex = 0;
+
+button.addEventListener("click", function () {
+  const imageUrl = imageUrls[currentIndex];
+
+  generateImage(imageUrl);
+
+  currentIndex = (currentIndex + 1) % imageUrls.length;
+});
+
+function generateImage(url) {
+  imageContainer.innerHTML = "";
+
+  const image = document.createElement("img");
+  image.src = url;
+  image.style.width = "400px"; 
+  image.style.height = "200px"; 
+  imageContainer.appendChild(image);
+}
